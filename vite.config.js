67ed3fc65e7cd769349f32525b1e50ceac61b7b0
B3nj4-u <pedro.ulloavalenzuela@gmail.com>
2024-05-30 11:12:08 -0400

    asd

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base:'https://b3nj4-u.github.io/portfolio/'
+})
